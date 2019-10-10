e7eae6a7deeca39b231f0cb591c59cd1b2fd22f2
JustGendron <justin.gendron44@gmail.com>
2019-10-10 10:45:02 +0200

    ajout script bdd

---
+++ resources/segabank.sql
@@ -0,0 +1,147 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le :  jeu. 10 oct. 2019 à 10:43
+-- Version du serveur :  10.4.6-MariaDB
+-- Version de PHP :  7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `segabank`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `agence`
+--
+
+CREATE TABLE `agence` (
+  `id` int(11) NOT NULL,
+  `code` int(11) NOT NULL,
+  `adresse` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `agence`
+--
+
+INSERT INTO `agence` (`id`, `code`, `adresse`) VALUES
+(1, 2, 'rue');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `compte`
+--
+
+CREATE TABLE `compte` (
+  `id` int(11) NOT NULL,
+  `solde` float NOT NULL,
+  `idagence` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `payant`
+--
+
+CREATE TABLE `payant` (
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `simple`
+--
+
+CREATE TABLE `simple` (
+  `id` int(11) NOT NULL,
+  `decouvert` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `agence`
+--
+ALTER TABLE `agence`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `compte`
+--
+ALTER TABLE `compte`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_idagence` (`idagence`);
+
+--
+-- Index pour la table `payant`
+--
+ALTER TABLE `payant`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `simple`
+--
+ALTER TABLE `simple`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `agence`
+--
+ALTER TABLE `agence`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `compte`
+--
+ALTER TABLE `compte`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `compte`
+--
+ALTER TABLE `compte`
+  ADD CONSTRAINT `FK_idagence` FOREIGN KEY (`idagence`) REFERENCES `agence` (`id`);
+
+--
+-- Contraintes pour la table `payant`
+--
+ALTER TABLE `payant`
+  ADD CONSTRAINT `payant_ibfk_1` FOREIGN KEY (`id`) REFERENCES `compte` (`id`);
+
+--
+-- Contraintes pour la table `simple`
+--
+ALTER TABLE `simple`
+  ADD CONSTRAINT `simple_ibfk_1` FOREIGN KEY (`id`) REFERENCES `compte` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
