8c7402f7aa6c29a61c25afed9a2d8b3bcdc817f5
JustGendron <justin.gendron44@gmail.com>
2019-10-13 16:38:24 +0200

    operation

--- src/jdbc/DAO/EpargneDAO.java
+++ src/jdbc/DAO/EpargneDAO.java
@@ -10,15 +10,16 @@ import java.util.List;
 public class EpargneDAO implements IDAO <Integer, Integer, Epargne> {
     private static final String INSERT_QUERY = "INSERT INTO epargne (id,tauxint) VALUES(?,?)";
     private static final String FIND_ALL_EPARGNE = "SELECT * FROM compte,epargne  WHERE compte.id = epargne.id";
+    private static final String FIND_QUERY_CODE = "SELECT * FROM compte,epargne  WHERE compte.id = epargne.id and compte.code = ?";
 
     @Override
     public void create(Epargne compteE) throws SQLException, IOException, ClassNotFoundException {
         Connection connection = PersistanceManager.getConnection();
-        if ( connection != null ) {
-            try ( PreparedStatement ps = connection
-                    .prepareStatement( INSERT_QUERY, Statement.RETURN_GENERATED_KEYS ) ) {
-                ps.setInt( 1, compteE.getId());
-                ps.setFloat( 2, compteE.getTauxInteret() );
+        if (connection != null) {
+            try (PreparedStatement ps = connection
+                    .prepareStatement(INSERT_QUERY, Statement.RETURN_GENERATED_KEYS)) {
+                ps.setInt(1, compteE.getId());
+                ps.setFloat(2, compteE.getTauxInteret());
 
                 ps.executeUpdate();
 
@@ -36,18 +37,18 @@ public class EpargneDAO implements IDAO <Integer, Integer, Epargne> {
     public List<Epargne> findAll() throws SQLException, IOException, ClassNotFoundException {
         List<Epargne> list = new ArrayList<>();
         Connection connection = PersistanceManager.getConnection();
-        if ( connection != null ) {
-            try ( PreparedStatement ps = connection.prepareStatement( FIND_ALL_EPARGNE ) ) {
-                try ( ResultSet rs = ps.executeQuery() ) {
-                    while ( rs.next() ) {
+        if (connection != null) {
+            try (PreparedStatement ps = connection.prepareStatement(FIND_ALL_EPARGNE)) {
+                try (ResultSet rs = ps.executeQuery()) {
+                    while (rs.next()) {
                         Epargne epargne = new Epargne();
-                        epargne.setCode( rs.getInt( "code" ) );
-                        epargne.setSolde( rs.getFloat( "solde" ) );
-                        epargne.setIdagence( rs.getInt( "idagence" ) );
-                        epargne.setTauxInteret( rs.getInt( "tauxint" ) );
+                        epargne.setCode(rs.getInt("code"));
+                        epargne.setSolde(rs.getFloat("solde"));
+                        epargne.setIdagence(rs.getInt("idagence"));
+                        epargne.setTauxInteret(rs.getInt("tauxint"));
 
 
-                        list.add( epargne );
+                        list.add(epargne);
                     }
                 }
             }
@@ -67,7 +68,22 @@ public class EpargneDAO implements IDAO <Integer, Integer, Epargne> {
     }
 
     @Override
-    public Epargne findByCode(Integer integer) throws SQLException, IOException, ClassNotFoundException {
-        return null;
+    public Epargne findByCode(Integer code) throws SQLException, IOException, ClassNotFoundException {
+        Epargne epargne = null;
+        Connection connection = PersistanceManager.getConnection();
+        if (connection != null) {
+            try (PreparedStatement ps = connection.prepareStatement(FIND_QUERY_CODE)) {
+                ps.setLong(1, code);
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        epargne = new Epargne();
+                        epargne.setSolde(rs.getFloat("solde"));
+                        epargne.setIdagence(rs.getInt("idagence"));
+
+                    }
+                }
+            }
+        }
+        return epargne;
     }
 }
